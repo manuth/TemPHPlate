96edd7dff89a0960cae0671cb119dc4cf724b16a
Manuel Thalmann <m@nuth.ch>
2017-06-15 13:16:39 +0200

    Add languages to the settings-class.

--- Properties/Settings.php
+++ Properties/Settings.php
@@ -1,8 +1,26 @@
 <?php
-    namespace manuth\TemPHPlate\Properties;
+    namespace ManuTh\TemPHPlate\Properties;
     {
+        /**
+         * Provides settings for the Project
+         */
         class Settings
         {
+            /**
+             * @var string[]
+             * The locales to use to execute php
+             */
+            public static $Locales = array
+            (
+                // Windows-Format
+                'de-CH',
+                'de-DE',
+                // Linux-Format
+                'de_CH',
+                'de_DE',
+                'de_CH.UTF8',
+                'de_DE.UTF8'
+            );
         }
     }
 ?>
