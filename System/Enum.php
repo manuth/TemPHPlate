0aa1750c920209ab00d44db09db983b2a5853e9e
Manuel Thalmann <m@nuth.ch>
2017-10-04 22:40:36 +0200

    Extend Enum-class

--- System/Enum.php
+++ System/Enum.php
@@ -38,6 +38,67 @@
             /**
              * @ignore
              */
+            private function setValue($value)
+            {
+                $this->Value = $value;
+            }
+
+            /**
+             * Determines whether one or more bit fields are set in the current instance.
+             *
+             * @param mixed $flag
+             * An enumeration value.
+             * 
+             * @return bool
+             * **true** if the bit field or bit fields that are set in _flag_ are also set in the current instance; otherwise, **false**.
+             */
+            public function HasFlag($flag)
+            {
+                if ($flag instanceof Enum)
+                {
+                    $flag = $flag->Value;
+                }
+
+                return ($this->Value & $flag) == $flag;
+            }
+
+            /**
+             * Adds a flag to the current instance.
+             *
+             * @param mixed $flag
+             * An enumeration value.
+             * 
+             * @return void
+             */
+            public function SetFlag($flag)
+            {
+                if ($flag instanceof Enum)
+                {
+                    $flag = $flag->Value;
+                }
+
+                $this->Value |= $flag;
+            }
+            
+            /**
+             * Unsets a flag from the current instance.
+             *
+             * @param mixed $flag
+             * An enumeration value.
+             */
+            public function ClearFlag($flag)
+            {
+                if ($flag instanceof Enum)
+                {
+                    $flag = $flag->Value;
+                }
+                
+                $this->Value &= ~$flag;
+            }
+
+            /**
+             * @ignore
+             */
             public function __InitializeStatic()
             {
                 $counter = 0;
@@ -70,6 +131,36 @@
                         $counter++;
                     }
                 }
+            }
+
+            /**
+             * Returns a string which represents the object.
+             *
+             * @return string
+             * A string that represents the current object.
+             */
+            public function ToString() : string
+            {
+                $result = '';
+                $values = array();
+
+                foreach ((new \ReflectionClass($this))->getProperties() as $property)
+                {
+                    /**
+                     * @var \ReflectionProperty $property
+                     */
+                    if ($property->isStatic())
+                    {
+                        $value = $property->getValue();
+                        
+                        if ($this->HasFlag($value))
+                        {
+                            $values[] = $property->getName();
+                        }
+                    }
+                }
+
+                return join(', ', $values);
             }
         }
     }
