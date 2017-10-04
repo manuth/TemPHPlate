9378f6aed6361d9ce50a3667db7f441c2cb85281
Manuel Thalmann <m@nuth.ch>
2017-10-04 21:52:25 +0200

    Add page Uni-Tests

--- UnitTests/Pages.php
+++ UnitTests/Pages.php
@@ -9,12 +9,25 @@
         Page,
         StyleSheet
     };
+    use ManuTh\TemPHPlate\Properties\Settings;
     {
-        $page = new Page();
-        $page->StyleDefinitions->Add(new StyleSheet('./markdown.css'));
-        $page->StyleDefinitions->Add(new StyleSheet('./tomorrow.css'));
-        $page->Title = "This is a test.";
+        echo '
+            <h2>Testing the <code>Page</code>-class</h2>';
 
-        echo $page->Draw();
+        global $page, $title, $head;
+
+        echo 'Creating a new page <code>$page</code>...<br />';
+
+        $page = new Page();
+        $title = 'This is a test';
+
+        RunTest('$page->StyleDefinitions->Add(new System\Web\StyleSheet("./markdown.css"))');
+        RunTest('$page->StyleDefinitions->Add(new System\Web\StyleSheet("./tomorrow.css"))');
+        RunTest('$page->Title = "'.$title.'"');
+        RunTest('strpos($page->Draw(), "<!DOCTYPE html>") !== false', true);
+        RunTest('$head = preg_replace("/[\\s\\S]*(<head>[\\s\\S]*<\/head>)[\\s\\S]*/m", "$1", $page->Draw())');
+        RunTest('strpos($head, "<title>'.$title.'</title>") !== false', true);
+        RunTest('$page->Locale->Name == System\Globalization\CultureInfo::GetCurrentCulture()->Name', true);
+        RunTest('strpos($head, "<link href=\\"./markdown.css\\" rel=\\"stylesheet\\" />") !== false', true);
     }
 ?>
