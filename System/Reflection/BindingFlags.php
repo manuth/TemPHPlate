ba918515d14694f62c1e3ec53e77a155f8fa79a3
Manuel Thalmann <m@nuth.ch>
2017-11-14 11:19:16 +0100

    Fix type-hints of enum-classes

--- System/Reflection/BindingFlags.php
+++ System/Reflection/BindingFlags.php
@@ -14,31 +14,43 @@
         {
             /**
              * Specifies that no binding flags are defined.
+             * 
+             * @var self
              */
             public static $Default = _BindingFlags::Default;
 
             /**
              * Specifies that the case of the member name should not be considered when binding.
+             * 
+             * @var self
              */
             public static $IgnoreCase = _BindingFlags::IgnoreCase;
 
             /**
              * Specifies that instance members are to be included in the search.
+             * 
+             * @var self
              */
             public static $Instance = _BindingFlags::Instance;
 
             /**
              * Specifies that non-public members are to be included in the search.
+             * 
+             * @var self
              */
             public static $NonPublic = _BindingFlags::NonPublic;
 
             /**
              * Specifies that public members are to be included in the search.
+             * 
+             * @var self
              */
             public static $Public = _BindingFlags::Public;
 
             /**
              * Specifies that static members are to be included in the search.
+             * 
+             * @var self
              */
             public static $Static = _BindingFlags::Static;
         }
